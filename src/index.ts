d7f0b8c6f9da9fb83e48ef23fc89d96974351247
Demian Parkhomenko <95881717+DemianParkhomenko@users.noreply.github.com>
2024-05-07 17:07:31 +0300

    Add `createClientMonoAcquiring`

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import createClient from 'openapi-fetch';
+import type { paths as pathsAcquiring } from './generated/acquiring.js';
+
+export type ClientOptions = Parameters<typeof createClient>[0];
+
+export const createClientMonoAcquiring = ({
+  clientOptions,
+}: {
+  clientOptions: ClientOptions;
+}) => {
+  return createClient<pathsAcquiring>({
+    baseUrl: 'https://api.monobank.ua',
+    ...clientOptions,
+  });
+};
